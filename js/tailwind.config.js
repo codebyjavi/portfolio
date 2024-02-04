002b84226e596dc6297aa7daff9fc97b438f0864
javierspr <learn.javier@gmail.com>
2024-02-04 17:52:03 -0300

    estructura basica de las secciones estilado con tailwind

---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    theme: {
+      colors: {
+        ...colors, // Include default colors (optional)
+        '': '',
+        
+      },
+    },
+  };
+  
