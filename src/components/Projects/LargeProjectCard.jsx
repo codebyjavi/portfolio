c3a73362a6372b50a03141dc49e7f4cb730431e0
javierspr <learn.javier@gmail.com>
2024-03-24 09:26:08 -0300

    update new large projects, LargeProjectCard

--- src/components/Projects/LargeProjectCard.jsx
+++ src/components/Projects/LargeProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function LargeProjectCard({live, srcImg, h3, paragraph, repo}) {
+function LargeProjectCard({live, repo, srcImg, iClassName1, iClassName2, iClassName3, h3, paragraph}){
+
   return (
     <div className="grid md:grid-cols-2 gap-4 text-white">
         <a href={live} target="_blank">
@@ -10,9 +11,9 @@ function LargeProjectCard({live, srcImg, h3, paragraph, repo}) {
             <h3 className="text-black dark:text-neutral-200 text-2xl md:text-3xl font-bold">{h3}</h3>
             <p className="text-black dark:text-neutral-200 ">{paragraph}</p>
             <div className="flex gap-2">
-                <i className="fa-brands fa-html5 text-[#FF8A00] text-lg md:text-2xl"></i>
-                <i className="fa-brands fa-css3-alt text-blue-500 text-lg md:text-2xl"></i>
-                <i className="fa-brands fa-js text-[#FFD747] text-lg md:text-2xl"></i>
+                <i className={iClassName1}></i>
+                <i className={iClassName2}></i>
+                <i className={iClassName3}></i>
             </div>
             <div className="flex gap-2">
                 <a className="text-black hover:scale-[1.02] font-bold py-1 px-3 my-auto bg-[#dadada] rounded-lg" href={repo} target="_blank">
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ecommercePhoto from "../../images/ecommerce.png"
+import reactEcommerce from "../../images/react-ecommerce.png"
+import guitarLA from "../../images/guitarLA.png"
 import ProjectCard from './ProjectCard'
 import LargeProjectCard from './LargeProjectCard'
 
@@ -8,14 +10,39 @@ function Proyectos() {
     <article className="mb-10 md:mb-20 px-3 md:px-0">
       <h2 className="text-yellow-500 font-bold text-2xl md:text-3xl mb-10">Proyectos <i className="fa-solid fa-code"></i></h2>
       <div className="flex flex-col gap-20 mb-20">
+    
+        <LargeProjectCard
+          live="https://guitar-react-javi.netlify.app/"
+          repo="https://github.com/javxdev/guitar-react"
+          srcImg={guitarLA}
+          iClassName1="bx bxl-tailwind-css text-[#19B5B9] text-lg md:text-2xl"
+          iClassName2="fa-brands fa-react text-[#64C7FF] text-lg md:text-2xl"
+          iClassName3="fa-brands fa-js text-[#FFD747] text-lg md:text-2xl"
+          h3="Venta de guitarras"
+          paragraph="Obtiene los productos de un array local, posee carrito de compras y utiliza el poder de react de reutilizar componentes."
+        />
+
+        <LargeProjectCard
+          live="https://ecommerce-react-javxdev.netlify.app/"
+          repo="https://github.com/javxdev/ecommerce-react"
+          srcImg={reactEcommerce}
+          iClassName1="bx bxl-tailwind-css text-[#19B5B9] text-lg md:text-2xl"
+          iClassName2="fa-brands fa-react text-[#64C7FF] text-lg md:text-2xl"
+          iClassName3="fa-brands fa-js text-[#FFD747] text-lg md:text-2xl"
+          h3="Ecommerce con React"
+          paragraph="Obtiene los productos de una API, posee distintas secciones y su carrito de compras."
+        />
 
         <LargeProjectCard
           live="https://ecommerceteam.netlify.app/"
-          srcImg={ecommercePhoto}
-          h3="Ecommerce"
-          paragraph="Obtiene los productos de una API, posee distintas secciones y su carrito de compras."
           repo="https://github.com/javier-it/proyecto-CODO"
-        />
+          srcImg={ecommercePhoto}
+          iClassName1="fa-brands fa-html5 text-[#FF8A00] text-lg md:text-2xl"
+          iClassName2="fa-brands fa-css3-alt text-blue-500 text-lg md:text-2xl"
+          iClassName3="fa-brands fa-js text-[#FFD747] text-lg md:text-2xl"
+          h3="Ecommerce con JavaScript"
+          paragraph="Obtiene los productos de una API, posee distintas secciones y su carrito de compras."
+          />
 
       </div>
       <div className="grid md:grid-cols-2 gap-4">
