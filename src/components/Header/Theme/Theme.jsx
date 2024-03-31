f422bc160c18a828480f97235b4313e91bd042c1
javierspr <learn.javier@gmail.com>
2024-03-30 22:37:24 -0300

    add local storage

--- src/components/Header/Theme/Theme.jsx
+++ src/components/Header/Theme/Theme.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import{ useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 function ThemeBtn() {
     const [theme, setTheme] = useState(() => {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      return "dark";
-    }
-    return "light";
+        const storedTheme = localStorage.getItem('theme');
+        return storedTheme ? storedTheme : (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
     });
 
     useEffect(() => {
-    if (theme === "dark") {
-      document.querySelector("html").classList.add("dark");
-    } else {
-      document.querySelector("html").classList.remove("dark");
-    }
+        if (theme === "dark") {
+            document.querySelector("html").classList.add("dark");
+        } else {
+            document.querySelector("html").classList.remove("dark");
+        }
+        localStorage.setItem('theme', theme);
     }, [theme]);
 
     function handleTheme() {
         setTheme(theme => (theme === "light" ? "dark" : "light"));
     }
 
-  return (
-    <button onClick={handleTheme} className="flex justify-center items-center p-3 bg-transparent text-4xl pointer">
-    {theme === "light" ? (
-      <i className="bx bx-moon text-black"></i>
-    ) : (
-      <i className="bx bx-sun text-white"></i>
-    )}
-    </button>
-  )
+    return (
+        <button onClick={handleTheme} className="flex justify-center items-center p-3 bg-transparent text-4xl pointer">
+            {theme === "light" ? (
+                <i className="bx bx-moon text-black"></i>
+            ) : (
+                <i className="bx bx-sun text-white"></i>
+            )}
+        </button>
+    )
 }
 
-export default ThemeBtn
+export default ThemeBtn;
